f8aa65479f1bd22c05a163929dbb02e6bb595d64
qquliyevvv <quluzade228@gmail.com>
2022-11-11 13:21:57 +0400

    Toqrul10

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ const sort =document.querySelector('.sort');
 const sort2 =document.querySelector('.sort2');
 const addbut = document.querySelector('.add');
 const input = document.querySelector('.list');
-const typex = document.querySelector('.type');
-const del = document.querySelectorAll('.x');
+const typex = document.querySelector('.typenew');
+let del = document.querySelector('.x2');
 
 function changebut() {
 sort.style.display = 'none';
@@ -18,8 +18,17 @@ sort2.addEventListener('click',changebut2);
 function add() {
 
      let p=document.createElement("p")
-     p.innerHTML = '<div class="type"><input class="list-text" type="text" ><button class="x"><img src="./images/x.svg" class="x-image" alt="Not found" onmouseover=this.src="./images/x2.svg" class="x2-image" alt="Not found" onmouseout=this.src="./images/x.svg" alt="Not found"></button></div>';
+     p.innerHTML = '<input class="list-text" type="text" ><button class="x2"><img src="./images/x.svg" class="x-image" alt="Not found" onmouseover=this.src="./images/x2.svg" class="x2-image" alt="Not found" onmouseout=this.src="./images/x.svg" alt="Not found"></button>';
+     p.className = "type typenew";
     input.appendChild(p);
-     }
+    del = document.querySelectorAll('.x2');  
+    removeFunc();
+}    
 addbut.addEventListener('click',add);
-del.addEventListener('click',(event) => { event.target.parentElement.parentElement.remove();});
+function removeFunc() {
+        del.forEach((item) => {
+            item.addEventListener('click', (event)=> {
+            event.target.parentElement.parentElement.remove();
+        });
+});
+}
