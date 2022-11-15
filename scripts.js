28c808c78d09983fe5876af8db66eb5200b7153b
qquliyevvv <quluzade228@gmail.com>
2022-11-16 03:06:39 +0400

    Toqrul15

--- scripts.js
+++ scripts.js
@@ -4,10 +4,10 @@ const addbut = document.querySelector('.add');
 const input = document.querySelector('.list');
 const typex = document.querySelector('.typenew');
 let listtxt =document.querySelectorAll('.list-text');
-let del = document.querySelector('.x2');
-let xr = document.querySelector('.x');
-let text=document.querySelector('.list');
-new Sortable(text);
+let del = document.querySelectorAll('.x');
+var a=1;
+console.log(a.length)
+new Sortable(input);
 function changebut() {
 sort.style.display = 'none';
 sort2.style.display = 'flex';
@@ -21,28 +21,43 @@ sort2.addEventListener('click',changebut2);
 function add() {
   
   let p=document.createElement("p")
-  p.innerHTML = '<input class="list-text" type="text" ><button class="x2"><img src="./images/x.svg" class="x-image" alt="Not found" onmouseover=this.src="./images/x2.svg" class="x2-image" alt="Not found" onmouseout=this.src="./images/x.svg" alt="Not found"></button>';
+  p.innerHTML = '<input class="list-text" type="text" ><button class="x"><img src="./images/x.svg" class="x-image" alt="Not found" onmouseover=this.src="./images/x2.svg" class="x2-image" alt="Not found" onmouseout=this.src="./images/x.svg" alt="Not found"></button>';
   p.className = "type typenew";
   input.appendChild(p);
-  del = document.querySelectorAll('.x2');  
+  del = document.querySelectorAll('.x');  
   console.log(document.querySelectorAll('.type'));
   input.scrollBy(0,10000);
-  removeFunc();
+  a++;
 }    
 addbut.addEventListener('click',add);
-function removeFunc() {
-        del.forEach((item) => {
-          // if(item)
-          item.addEventListener('click', (event)=> {
-            event.target.parentElement.parentElement.remove();
-          });
-        });
-      }
-      xr.onclick = function() {
+del.forEach((item)=>{
+item.onclcik =  function(event){
+if(listtxt.length>1){
+   if(event.target.parentElement != '' && event.target.parentElement.readOnly==true){
+    event.target.parentElement.parentElement.remove();
+   }
+   else{
+    event.target.parentElement.readOnly=false;
+    event.target.parentElement.value='';    
+   }
+}
+else{
+    listtxt.value='';
+    listtxt.readOnly=false;
+}
+}})
+// function removeFunc() {
+//         del.forEach((item) => {
+//           item.addEventListener('click', (event)=> {
+//             event.target.parentElement.parentElement.remove();
+//           });
+//         });
+//       }
+  //     xr.onclick = function() {
         
-        document.querySelector(".input1").value = "";
-        document.querySelector(".input1").readOnly=false;
-  }
+  //       document.querySelector(".input1").value = "";
+  //       document.querySelector(".input1").readOnly=false;
+  // }
   input.addEventListener('keydown', function(event) {
     if (event.code == 'Enter') {
         input.scrollBy(0,10000);
