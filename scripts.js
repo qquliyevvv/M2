7b465de6ca5d897dcd4fecf40ba5bd6daed47191
qquliyevvv <quluzade228@gmail.com>
2022-11-15 10:35:15 +0400

    Toqrul14

--- scripts.js
+++ scripts.js
@@ -14,33 +14,36 @@ sort2.style.display = 'flex';
 }
 sort.addEventListener('click',changebut);
 function changebut2() {
-    sort.style.display = 'flex';
-    sort2.style.display = 'none';
-    }
+  sort.style.display = 'flex';
+  sort2.style.display = 'none';
+}
 sort2.addEventListener('click',changebut2);
 function add() {
-
-     let p=document.createElement("p")
-     p.innerHTML = '<input class="list-text" type="text" ><button class="x2"><img src="./images/x.svg" class="x-image" alt="Not found" onmouseover=this.src="./images/x2.svg" class="x2-image" alt="Not found" onmouseout=this.src="./images/x.svg" alt="Not found"></button>';
-     p.className = "type typenew";
-    input.appendChild(p);
-    del = document.querySelectorAll('.x2');  
-    removeFunc();
+  
+  let p=document.createElement("p")
+  p.innerHTML = '<input class="list-text" type="text" ><button class="x2"><img src="./images/x.svg" class="x-image" alt="Not found" onmouseover=this.src="./images/x2.svg" class="x2-image" alt="Not found" onmouseout=this.src="./images/x.svg" alt="Not found"></button>';
+  p.className = "type typenew";
+  input.appendChild(p);
+  del = document.querySelectorAll('.x2');  
+  console.log(document.querySelectorAll('.type'));
+  removeFunc();
+  // console.log(inputs)
+  // console.log(sortarr)
 }    
 addbut.addEventListener('click',add);
 function removeFunc() {
         del.forEach((item) => {
-            item.addEventListener('click', (event)=> {
+          item.addEventListener('click', (event)=> {
             event.target.parentElement.parentElement.remove();
+          });
         });
-});
-}
-xr.onclick = function() {
-
-    document.querySelector(".input1").value = "";
-    document.querySelector(".input1").readOnly=false;
+      }
+      xr.onclick = function() {
+        
+        document.querySelector(".input1").value = "";
+        document.querySelector(".input1").readOnly=false;
   }
-input.addEventListener('keydown', function(event) {
+  input.addEventListener('keydown', function(event) {
     if (event.code == 'Enter') {
         input.scrollBy(0,10000);
          listtxt =document.querySelectorAll('.list-text');
@@ -48,18 +51,53 @@ input.addEventListener('keydown', function(event) {
        item.readOnly=true;
 })
     }});
-let sortarr=[...document.querySelectorAll('.type')];
+
 function sortbyin(){
-sortarr=[...document.querySelectorAll('.type')].sort((a, b) => {
-    if (a.innerText < b.innerText) {
+  let inputs = document.querySelectorAll('input');
+let sortarr=[]
+
+for(let i = 0; i < inputs.length; i++){
+  sortarr.push(inputs[i].value);
+}
+  
+  sortarr.sort((a, b) => {
+    if (a < b) {
       return -1;
     }
-    if (a.innerText > b.innerText) {
+    if (a > b) {
       return 1;
     }
-  
     return 0;
-  });}
-sort.addEventListener('click', () => {
-    sortarr.forEach(elem => input.append(elem));
-});
+  });
+  
+  for(let i = 0; i < inputs.length; i++){
+    inputs[i].value = sortarr[i];
+  }
+ 
+}
+sort.addEventListener('click', sortbyin);
+
+function sortbydrop(){
+let inputs = document.querySelectorAll('input')
+let sortarr=[]
+
+for(let i = 0; i < inputs.length; i++){
+  sortarr.push(inputs[i].value);
+}
+  
+  sortarr.sort((a, b) => {
+    if (a < b) {
+      return 1;
+    }
+    if (a > b) {
+      return -1;
+    }
+    return 0;
+  });
+  
+  for(let i = 0; i < inputs.length; i++){
+    inputs[i].value = sortarr[i];
+  }
+ 
+}
+sort2.addEventListener('click', sortbydrop);
