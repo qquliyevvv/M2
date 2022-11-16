c81428f219cb495ac56cb856f3a073ac22f0874c
qquliyevvv <quluzade228@gmail.com>
2022-11-16 14:20:36 +0400

    Toqrul17

--- scripts.js
+++ scripts.js
@@ -4,10 +4,10 @@ const addbut = document.querySelector('.add');
 const input = document.querySelector('.list');
 const typex = document.querySelector('.typenew');
 let listtxt =document.querySelectorAll('.list-text');
-let del = document.querySelectorAll('.x');
-var a=1;
-console.log(a.length)
-new Sortable(input);
+let del = document.querySelector('.x2');
+let xr = document.querySelector('.x');
+let text=document.querySelector('.list');
+new Sortable(text);
 function changebut() {
 sort.style.display = 'none';
 sort2.style.display = 'flex';
@@ -21,43 +21,37 @@ sort2.addEventListener('click',changebut2);
 function add() {
   
   let p=document.createElement("p")
-  p.innerHTML = '<input class="list-text" type="text" ><button class="x"><img src="./images/x.svg" class="x-image" alt="Not found" onmouseover=this.src="./images/x2.svg" class="x2-image" alt="Not found" onmouseout=this.src="./images/x.svg" alt="Not found"></button>';
+  p.innerHTML = '<input class="list-text" type="text" ><button class="x2"><img src="./images/x.svg" class="x-image" alt="Not found" onmouseover=this.src="./images/x2.svg" class="x2-image" alt="Not found" onmouseout=this.src="./images/x.svg" alt="Not found"></button>';
   p.className = "type typenew";
   input.appendChild(p);
-  del = document.querySelectorAll('.x');  
+  del = document.querySelectorAll('.x2');  
   console.log(document.querySelectorAll('.type'));
   input.scrollBy(0,10000);
-  a++;
+  removeFunc();
 }    
 addbut.addEventListener('click',add);
-del.forEach((item)=>{
-item.onclcik =  function(event){
-if(listtxt.length>1){
-   if(event.target.parentElement != '' && event.target.parentElement.readOnly==true){
-    event.target.parentElement.parentElement.remove();
-   }
-   else{
-    event.target.parentElement.readOnly=false;
-    event.target.parentElement.value='';    
-   }
+function removeFunc() {
+  del.forEach((item) => {
+    item.addEventListener('click', (event)=> {
+      console.log(event.target.parentElement.parentElement.querySelector(".list-text").value );
+      if(event.target.parentElement.parentElement.querySelector(".list-text").value !='' && event.target.parentElement.parentElement.querySelector(".list-text").readOnly != true){
+        event.target.parentElement.parentElement.querySelector(".list-text").value = "";
+      }
+      else if(event.target.parentElement.parentElement.querySelector(".list-text").readOnly == true && event.target.parentElement.parentElement.querySelector(".list-text").value !='') {
+        event.target.parentElement.parentElement.remove();
+      }
+      else{
+
+        event.target.parentElement.parentElement.remove();
+      }
+    });
+  });
 }
-else{
-    listtxt.value='';
-    listtxt.readOnly=false;
-}
-}})
-// function removeFunc() {
-//         del.forEach((item) => {
-//           item.addEventListener('click', (event)=> {
-//             event.target.parentElement.parentElement.remove();
-//           });
-//         });
-//       }
-  //     xr.onclick = function() {
+      xr.onclick = function() {
         
-  //       document.querySelector(".input1").value = "";
-  //       document.querySelector(".input1").readOnly=false;
-  // }
+        document.querySelector(".input1").value = "";
+        document.querySelector(".input1").readOnly=false;
+  }
   input.addEventListener('keydown', function(event) {
     if (event.code == 'Enter') {
         input.scrollBy(0,10000);
